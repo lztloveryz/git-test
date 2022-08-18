3ddb2181f994b0393f067f26220edc2d1c43e5b1
LZT <LZT@.com>
2022-08-18 09:24:13 +0800

    hot-fix commit

--- src/main/java/com/lzt/git/gitTest.java
+++ src/main/java/com/lzt/git/gitTest.java
@@ -2,7 +2,8 @@ package com.lzt.git;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
-        System.out.println("hello git!");
+        System.out.println("hello git! 1");
+        System.out.println("hello git! 2");
+        System.out.println("hello git! 3");
     }
 }
